93dc2c7f68abf13e5765b2faf9137b963491ebb5
maks byzovskiy <byzovskiy290808@gmail.com>
2024-03-24 12:46:06 +0100

    add likes, dislikes

--- apps/article/models.py
+++ apps/article/models.py
@@ -1,8 +1,3 @@
-from PIL import Image
-from django.core.files import File
-from io import BytesIO
-
-
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -12,34 +7,11 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='post_images')
-    thumbnail = models.ImageField(upload_to='post_images', null=True, blank=True)
+    like = models.ManyToManyField(User, related_name='like_posts', blank=True)
+    dislike = models.ManyToManyField(User, related_name='dislike_posts', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         ordering = ['-created_at']
-        
-    
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.image:
-            self.set_thumbnail()
-        super().save(*args, **kwargs)
-    
-    def set_thumbnail(self):
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-                
-            thumb_io = BytesIO()
-            img.save(thumb_io, format='UPEG', quality=85)
-                
-            file_name = self.image.name.split('/')[-1]
-            self.thumbnail.save(file_name, File(thumb_io), save=False)
-        
-    def get_thumbnail(self):
-        if not self.thumbnail:
-            self.set_thumbnail()
-        return self.thumbnail.url
                 
--- apps/article/views.py
+++ apps/article/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from .models import Post
 from .forms import PostForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -16,7 +17,7 @@ def index(request):
     }
     return render(request, 'article/index.html', context)
 
-
+@login_required
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     context = {
@@ -24,7 +25,7 @@ def detail(request, post_id):
     }
     return render(request, 'article/detail.html', context)
 
-
+@login_required
 def create_view(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -33,3 +34,25 @@ def create_view(request):
             post.author = request.user
             post.save()
             return redirect('article:detail', post_id=post.id)
+        
+        
+@login_required
+def like_view(request, post_id):
+    if request.method == 'GET':
+        post = get_object_or_404(Post, pk=post_id)
+        if request.user in post.like.all():
+            post.like.remove(request.user)
+        else:
+            post.like.add(request.user)
+        return JsonResponse({'like_count': post.like.count()})
+        
+        
+@login_required
+def dislike_view(request, post_id):
+    if request.method == 'GET':
+        post = get_object_or_404(Post, pk=post_id)
+        if request.user in post.dislike.all():
+            post.dislike.remove(request.user)
+        else:
+            post.dislike.add(request.user)
+        return JsonResponse({'dislike_count': post.dislike.count()})
